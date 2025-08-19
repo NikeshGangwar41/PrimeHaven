6e3578abbc3112f572eead0fc18339998b6770f0
Nikesh Gangwar <146576953+NikeshGangwar41@users.noreply.github.com>
2025-08-20 04:22:17 +0530

    splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    val nav_version = "2.9.3"
+
+    // Jetpack Compose integration
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
+
 }
